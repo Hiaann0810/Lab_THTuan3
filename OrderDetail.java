c5d407f811eecf130a5312b4ffb757e27ded9785
Hiaann0810 <125818312+Hiaann0810@users.noreply.github.com>
2023-05-09 10:46:11 +0700

    Add files via upload

---
+++ OrderDetail.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package th3.bai1;
+
+/**
+ *
+ * @author vietthanh
+ */
+public class OrderDetail {
+    private int quatity;
+    private Product product;
+
+    public OrderDetail(Product product, int quantity) {
+        this.product = product;
+        this.quatity = quatity;
+    }
+
+    public double calcTotalPrice() {
+        return this.quatity * this.product.getPrice();
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quatity = quatity;
+    }
+
+    public int getQuatity() {
+        return this.quatity;
+    }
+}
